decccf583c1536bd57883898a76c551815d62686
Furkan Karaca <33620468+layemutane@users.noreply.github.com>
2018-07-18 18:06:13 +0300

    Add files via upload

---
+++ Calışmalar2.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main ()
+{
+int i=0;
+	while (i<=10)
+	{ 
+      if (i%2==0)
+      
+      {
+      	printf ("Cift : ");
+      	printf ("%d\n",i);    	    
+	  }
+  	
+  		
+  		else 
+  		{
+  			printf ("Tek : ");
+  			printf ("%d\n",i);
+		  }
+		  
+		  	i++;
+	}
+
+}
